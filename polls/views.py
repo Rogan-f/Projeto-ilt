e53d7f0698cbb7e854992ee38379dd285ff574c9
fernanda <fernandarogan@gmail.com>
2023-08-30 09:33:49 -0300

    continuação 30/08

--- polls/views.py
+++ polls/views.py
@@ -21,15 +21,19 @@ def results(request, question_id):
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
 
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, ListView, DetailView
 from django.urls import reverse_lazy 
 
 class QuestionCreateView(CreateView):
     model = Question 
-    fields = ('question_text', 'pub_date')
-    success_url: reverse_lazy('index')
+    fields = ('question_text',)
+    success_url: reverse_lazy('question-list')
     template_name = 'polls/question_form.html'
 
 class QuestionListView(ListView): 
     model = Question
     context_object_name = 'questions'
+
+class QuestionDetailView(DetailView):
+    model = Question
+    context_object_name='question'
